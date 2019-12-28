e6a697794b9fe6fde078a9cefc0aa6f2bc4fb4ce
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-12-28 15:33:54 -0600

    Updates

---
+++ precache-manifest.224374d1b219770d038a4d39d3004819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b9a2820d198fb82c277ce217219c9d9",
+    "url": "/gift-cards-web/index.html"
+  },
+  {
+    "revision": "aa5b2f70903aa8987b3b",
+    "url": "/gift-cards-web/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "15e31b3d62416d285b04",
+    "url": "/gift-cards-web/static/css/main.57432353.chunk.css"
+  },
+  {
+    "revision": "aa5b2f70903aa8987b3b",
+    "url": "/gift-cards-web/static/js/2.5aa0b1db.chunk.js"
+  },
+  {
+    "revision": "15e31b3d62416d285b04",
+    "url": "/gift-cards-web/static/js/main.adc102bd.chunk.js"
+  },
+  {
+    "revision": "2dbe5adab797bbfdc5d7",
+    "url": "/gift-cards-web/static/js/runtime-main.3ade5b63.js"
+  }
+]);
