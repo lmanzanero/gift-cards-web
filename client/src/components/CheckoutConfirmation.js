ecac5d553afa09faa2afcc059a2bdf38badee007
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-12-30 00:16:14 -0600

    added button to checkout page and did final ui changes

--- client/src/components/CheckoutConfirmation.js
+++ client/src/components/CheckoutConfirmation.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 // import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import uuid from 'uuid';
 
@@ -10,8 +11,9 @@ export default class CheckOut extends Component {
                 <Row>
                     <Col xs="9"> 
                         <p className="title">Purchase Confirmation</p>
-                        <hr className="my-2"/>  
                         <div className="purchase-confirmation-box">
+                            <hr className="my-2"/>  
+                            <br/>
                             <Row>
                             <Col xs="3">
                                 <div className="hero-container">
@@ -25,15 +27,15 @@ export default class CheckOut extends Component {
                                         <div className="table-box-right"> 
                                             <table> 
                                                 <tr>
-                                                    <td className="title">To:</td>
+                                                    <td className="table-title">To:</td>
                                                     <td className="value">christine.wei1995@gmail.com</td>
                                                 </tr>
                                                 <tr>
-                                                    <td className="title">From:</td>
+                                                    <td className="table-title">From:</td>
                                                     <td className="value">Reggie Escobar</td>
                                                 </tr>  
                                                 <tr>
-                                                    <td className="title">Message:</td>
+                                                    <td className="table-title">Message:</td>
                                                     <td className="value">I hope your enjoy this gift!</td>
                                                 </tr>  
                                             </table> 
@@ -43,15 +45,15 @@ export default class CheckOut extends Component {
                                         <div className="table-box-right"> 
                                             <table> 
                                                 <tr>
-                                                    <td className="title">Amount:</td>
+                                                    <td className="table-title">Amount:</td>
                                                     <td className="value"> $25</td>
                                                 </tr>
                                                 <tr>
-                                                    <td className="title">Quantity:</td>
+                                                    <td className="table-title">Quantity:</td>
                                                     <td className="value">1</td>
                                                 </tr>  
                                                 <tr>
-                                                    <td className="title">Region:</td>
+                                                    <td className="table-title">Region:</td>
                                                     <td className="value">USA</td>
                                                 </tr>  
                                             </table> 
@@ -65,34 +67,37 @@ export default class CheckOut extends Component {
 
                     <Col xs="3"> 
                         <div className="order-details-box">
-                            <p className="title">Order Details</p>
+                            <p className="header-title">Order Details</p>
                             <div className="table-box-left">
                                 <hr className="my-2" />
                                 <table> 
                                     <tr>
-                                        <td>Face Value</td>
-                                        <td align="right">$25</td>
+                                        <td className="table-title">Face Value</td>
+                                        <td  className="value" align="right">$25</td>
                                     </tr>
                                     <tr>
-                                        <td>Quantity</td>
-                                        <td align="right">1x</td>
+                                        <td className="table-title">Quantity</td>
+                                        <td  className="value" align="right">1x</td>
                                     </tr>
                                     <hr className="my-2" />
                                     <tr>
-                                        <td>Sub Total</td>
-                                        <td align="right">$25</td>
+                                        <td className="table-title">Sub Total</td>
+                                        <td className="value" align="right">$25</td>
                                     </tr> 
                                     <tr>
-                                        <td>Service Fee & Tax</td>
-                                        <td align="right">$3</td>
+                                        <td className="table-title">Service Fee & Tax</td>
+                                        <td className="value" align="right">$3</td>
                                     </tr>
                                     <hr className="my-2"/>
                                     <tr>
-                                        <td>Total</td> 
+                                        <td className="table-title" style={{fontWeight: "bold"}}>Total</td> 
+                                        <td className="value" align="right">$28</td> 
                                     </tr>
                                 </table> 
                             </div>
                         </div>
+                        <br/>
+                        <Link to="/"><button className="main-button">Buy Now</button></Link>
                     </Col>
                 </Row>
             </Container>
