313ae83a4c73a768a66308dfd7b2074ecb6c694c
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-12-29 23:41:05 -0600

    Added social icons

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,14 +11,14 @@ export default class Footer extends Component {
                     <Col xs="3">
                         <p className="footer-logo-text">Powered By</p>
                         <div className="footer-logo">
-                        <a href="#"><img src="logo192.png"/></a>
+                        <a href="#"><img src="oblip_logo.png"/></a>
                         </div> 
                         <div className="footer-copy-right">
                             <p>Copyright @ giftcards Belize</p>
                             <div className="footer-social-logos">
-                               <a href="#"><img src="logo192.png" alt="facebook"/></a>
-                               <a href="#"><img src="logo192.png" alt="twitter"/></a>
-                               <a href="#"><img src="logo192.png" alt="instagram"/></a>
+                               <a href="#"><img src="facebook_logo.png" alt="facebook"/></a>
+                               <a href="#"><img src="twitter_logo.png" alt="twitter"/></a>
+                               <a href="#"><img src="instagram_logo.png" alt="instagram"/></a>
                             </div>
                         </div>
                     </Col>
