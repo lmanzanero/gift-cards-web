2e4ea89d658ab7dbe2eac837fb3e36ed887f0db3
Reggie Gian Escobar <contact@reggieescobar.com>
2020-01-13 23:31:02 -0600

    Added API endpoint to get a list of giftcards

--- routes/api/giftcards.js
+++ routes/api/giftcards.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 // @route GET /giftcards
 // @desc Gets a list of available giftcards
 // @access Public
 router.get('/', (req, res) => {
-  res.send({
-    statusCode: 200,
-    message: '/Giftcards'
+  const offset = Number(req.query.offset) || 0;
+  const url = `${process.env.GIFTCARD_API}/brands?region=2&currencyisocode=USD&offset=${offset}&limit=20`
+  axios({
+    method: 'GET',
+    url: url,
+    headers: {
+      'Authorization': `Bearer ${process.env.GIFTCARD_ACCESS_TOKEN}`
+    }
+  }).then((brands) => {
+    res.send(brands.data)
+  }).catch(error => {
+    let payload = {
+      httpStatus: error.status,
+      message: 'Something went wrong fetching giftcards'
+    }
+    res.send(payload)
   })
 });
 
