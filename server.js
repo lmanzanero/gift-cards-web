9749324df8ab06450c410b0504c9636ec37560b0
Reggie Gian Escobar <contact@reggieescobar.com>
2020-01-13 23:37:45 -0600

    Added basic rate limiting

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const rateLimit = require("express-rate-limit");
 
 // load environment variables in dev
 if (process.env.NODE_ENV !== 'production') {
@@ -13,11 +14,19 @@ const giftcards = require('./routes/api/giftcards')
 const orders = require('./routes/api/orders');
 
 
+// rate limiting config
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+
 // initialize app
 const app = express();
 
 // middlewares
 app.use(bodyParser.json());
+app.use(limiter);
 
 // connect to Mongo
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
