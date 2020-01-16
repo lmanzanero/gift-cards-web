50cbde3e0e5f785b3a6fe6d5abf855fa862c18e2
Reggie Gian Escobar <contact@reggieescobar.com>
2020-01-15 20:37:38 -0600

    Fixed header design bug

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -17,14 +17,14 @@ class AppNavbar extends Component {
   render () {
     return (
       <div>
-         <Container fluid={true}> 
+         <div> 
            <h1 className="main-title"><Link to="/">Gift Cards <br/>Belize</Link></h1>
            <p className="sub-title">Purchase cards with your Oblip account</p>
 
             {/* <div id="bread-crumbs">
               <sp>Home</p>
             </div> */}
-        </Container>
+        </div>
     </div>
     )
   }
